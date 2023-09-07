3923dbdda70354535741cbc7aa0b2b0a888363a0
Echo <102426116+Echo-Jie3721@users.noreply.github.com>
2023-09-07 21:19:58 +0800

    init repo

---
+++ src/test/java/echo/com/community_test/CommunityTestApplicationTests.java
@@ -0,0 +1,13 @@
+package echo.com.community_test;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CommunityTestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
